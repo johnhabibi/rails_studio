d21fff25ab852a4ea4a9a2994b04419a9c22c1d0
John Habibi <johnhabibi@gmail.com>
2018-02-27 22:03:40 -0600

    create one-to-many forms

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -7,15 +7,17 @@ class Movie < ApplicationRecord
 
   validates :image_file_name, allow_blank: true, format: {
     with:    /\w+\.(gif|jpg|png)\z/i,
-    message: "must reference a GIF, JPG, or PNG image"
+    message: 'must reference a GIF, JPG, or PNG image'
   }
 
-  RATINGS = %w(G PG PG-13 R NC-17)
+  RATINGS = %w[G PG PG-13 R NC-17].freeze
 
   validates :rating, inclusion: { in: RATINGS }
 
+  has_many :reviews, dependent: :destroy
+
   def self.released
-    where("released_on <= ?", Time.now).order("released_on desc")
+    where('released_on <= ?', Time.now).order('released_on desc')
   end
 
   def self.hits
@@ -31,6 +33,6 @@ class Movie < ApplicationRecord
   end
 
   def flop?
-    total_gross.blank? || total_gross < 50000000
+    total_gross.blank? || total_gross < 50_000_000
   end
 end
